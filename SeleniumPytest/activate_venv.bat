afe6e6ed180450bfb885acefe33b9da06685dc1b
sachdh-anand <sachdhanand.n@gmail.com>
2025-01-10 16:59:36 -0800

    Configured_env

---
+++ SeleniumPytest/activate_venv.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Navigate to the project directory
+cd C:\GitHub\TestOrbit\SeleniumPytest
+
+REM Activate the virtual environment
+call .\venv\Scripts\activate
---
+++ SeleniumPytest/conftest.py
@@ -0,0 +1,49 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils.config import Config
+
+
+@pytest.fixture(scope="function")
+def driver():
+    # Set up Chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-popup-blocking")
+
+    # Initialize WebDriver
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(Config.IMPLICIT_WAIT)
+
+    # Navigate to the URL with basic auth credentials
+    auth_url = f"https://{Config.USERNAME}:{Config.PASSWORD}@the-internet.herokuapp.com/basic_auth"
+    driver.get(auth_url)
+    yield driver
+
+    # Teardown
+    driver.quit()
+
+
+def pytest_html_report_title(report):
+    # Set a custom title for the HTML report
+    report.title = "TestOrbit's SeleniumPytest Testing Report"
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    # Add custom metadata to the HTML report
+    if not hasattr(config, "workerinput"):  # Avoid adding metadata for distributed tests
+        config._metadata = {
+            "Project Name": "TestOrbit",
+            "Module": "SeleniumPytest",
+            "Base URL": Config.BASE_URL,
+            "Browser": Config.BROWSER,
+            "Tester": "Your Name",
+        }
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_html_results_summary(prefix, summary, postfix):
+    # Add a custom message in the summary section
+    prefix.extend(["<p style='color: blue;'>This is an automated test report for TestOrbit's SeleniumPytest framework.</p>"])
